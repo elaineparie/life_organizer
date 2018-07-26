68ebd6af5db0d35ba16c5e457af6d310c5dd2eea
Elaine Anderson <eparie@bu.edu>
2018-07-26 15:57:41 +0000

    add to bag routes

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -5,4 +5,18 @@ class ListController  < ApplicationController
     erb :'lists/index'
   end
 
+get "/lists/new" do
+  erb :'lists/new'
+end
+
+get "/lists/:id" do
+  @list = List.find(params[:id])
+  erb :'lists/show'
+end
+
+post "/lists" do
+    List.create(params)
+    redirect "/lists"
+end
+
 end
