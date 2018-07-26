c774eebbcf557326d263ed30ee35ab17b7b0c7cb
Elaine Anderson <eparie@bu.edu>
2018-07-26 15:44:24 +0000

    add login route and form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -15,4 +15,23 @@ class UserController  < ApplicationController
   end
 end
 
+get '/login' do
+    #if !session[:user_id]
+      erb :'users/login'
+  #  else
+  #    redirect '/lists'
+  #  end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/lists"
+    else
+      redirect to '/signup'
+    end
+  end
+
+
 end
