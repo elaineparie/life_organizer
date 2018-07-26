b8123743f19c2d6456a9f3c9bf74a53367bebd9f
Elaine Anderson <eparie@bu.edu>
2018-07-26 18:07:41 +0000

    saftey

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,3 +1,18 @@
-class TaskController  < ApplicationController 
+class TaskController  < ApplicationController
+
+  get "/tasks" do
+    @tasks = Task.all
+    erb :'tasks/index'
+  end
+
+  get "/tasks/new" do
+    @lists = List.all
+  erb :'tasks/new'
+end
+
+post "/tasks" do
+  @task = Task.create(params)
+  redirect "/lists/#{@task.list.id}"
+end
 
 end
