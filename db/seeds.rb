674c3c5f10fc141c5009f385d410ba9e0ea81f3e
Elaine Anderson <eparie@bu.edu>
2018-07-26 14:53:48 +0000

    seed data

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+
+User.create(username: "lordgains", password: "Temp444")
+User.create(username: "elaineparie", password: "hithere")
+
+List.create(name: "School")
+List.create(name: "Cleaning")
+List.create(name: "Food")
+
+List.create(name: "Work")
+List.create(name: "Family")
+List.create(name: "Appointments")
+
+Task.create(name: "Family reunion this Sunday!", importance: 6)
+Task.create(name: "Answer all emails", importance: 9)
+Task.create(name: "Find a Primary Care", importance: 7)
+
+Task.create(name: "History Assignment", importance: 10)
+Task.create(name: "Clear out closet", importance: 5)
+Task.create(name: "Buy bananas", importance: 5)
