fb83c487801ad7b05ec1f81d514f9383e9d9666b
Elaine Anderson <eparie@bu.edu>
2018-07-27 14:58:08 +0000

    saftey for now

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :list
 
-  def self.valid_params?(params)
-  return !params[:task][:name].empty? && !params[:task][:list_id].empty?
+  def self.not_valid_params?(params)
+  return params[:task][:name].empty? || !params[:task][:list_id].empty? || !params[:list][:name].empty?
 end
 end
